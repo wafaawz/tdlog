0b5f79c3616bd407e750e95d729b5d029b2b8832
Manal1310 <114986221+Manal1310@users.noreply.github.com>
2022-11-03 21:35:50 +0000

    Update weapon.py

--- weapon.py
+++ weapon.py
@@ -3,5 +3,41 @@ class Weapon:
         self._ammunitions=ammunitions
         self._range=range
     def fire_at(self,x,y,z):
-        print( "the weapon will fire at "(x,y,z))
-        print('a')
+        print( "the weapon will fire at ",(x,y,z))
+        
+class LMAS(Weapon):
+    def __init__(self):
+        Weapon.__init__(self,40,30)
+    def fire_at(self,x,y,z):
+        if self._ammunitions==0:
+            print('NoAmmunitionError')
+        elif z!=0:
+            self._ammunitions-=1
+            print("OutofRangeError")
+        else:
+            print("The Lance-missiles antisurface will fire at",(x,y,z))
+            
+class LMAA(Weapon):
+    def __init__(self):
+        Weapon.__init__(self,50,40)
+    def fire_at(self,x,y,z):
+        if self._ammunitions==0:
+            print('NoAmmunitionError')
+        elif z<=0:
+            self._ammunitions-=1
+            print("OutofRangeError")
+        else:
+            print("The Lance-missiles anti-air will fire at",(x,y,z))
+            
+class LT(Weapon):
+    def __init__(self):
+        Weapon.__init__(self,15,20)
+    def fire_at(self,x,y,z):
+        if self._ammunitions==0:
+            print('NoAmmunitionError')
+        elif z>0:
+            self._ammunitions-=1
+            print("OutofRangeError")
+        else:
+            print("The Lance-torpilles will fire at",(x,y,z))
+            
