36a01b9b0c329036092f3b7ea7b55c0d7cd5a75e
Manal1310 <114986221+Manal1310@users.noreply.github.com>
2022-11-03 22:22:21 +0000

    Update vessel.py

--- vessel.py
+++ vessel.py
@@ -108,33 +108,7 @@ class Aircraft(Vessel):
             print("OutOfRangeError")
             lmas1._ammunitions-=1
             
-class Espace:
-    def __init__(self,x,y,z,listeV):
-        if 0<=x<=100 and 0<=y<=100 and (z==0 or z==1 or z==-1):
-            self._x=x
-            self._y=y
-            self._z=z
-        self._listeV=listeV
-    
-    def ajouter(self,V):
-        a=0
-        c=0
-        for j in range(0,len(self._listeV)):
-            a+=j._max_hits
-            if j.getcoordinates()==V.getcoordinates():
-                c=1
-        if c==0 and a<=22:
-            self._listeV = self._listeV + [V]
-    
-    def recevoircoup(self,x,y,z):
-        b=0
-        for k in range(0,len(self._listeV)):
-            if k.getcoordinates()==(x,y,z):
-                b=1
-        if b==1:
-            return(True)
-        else :
-            return(False)
+
         
         
                 
