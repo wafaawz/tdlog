e769cedf994eb9f4f2f4975fc3f3c57e39475822
Manal1310 <114986221+Manal1310@users.noreply.github.com>
2022-11-04 22:00:53 +0000

    Update espace.py

--- espace.py
+++ espace.py
@@ -1,3 +1,14 @@
+from weapon import Weapon
+from weapon import LT
+from weapon import LMAA
+from weapon import LMAS
+from vessel import Vessel
+from vessel import Cruiser
+from vessel import Fregate
+from vessel import Submarine
+from vessel import Destroyer
+from vessel import Aircraft
+
 
 class Espace:
     def __init__(self,x,y,z,listeV):
@@ -11,8 +22,8 @@ class Espace:
         a=0
         c=0
         for j in range(0,len(self._listeV)):
-            a+=j._max_hits
-            if j.getcoordinates()==V.getcoordinates():
+            a+=(self._listeV)[j]._max_hits
+            if (self._listeV)[j].get_coordinates()==V.get_coordinates():
                 c=1
         if c==0 and a<=22:
             self._listeV = self._listeV + [V]
@@ -20,7 +31,7 @@ class Espace:
     def recevoircoup(self,x,y,z):
         b=0
         for k in range(0,len(self._listeV)):
-            if k.getcoordinates()==(x,y,z):
+            if (self._listeV)[k].get_coordinates()==(x,y,z):
                 b=1
         if b==1:
             return(True)
